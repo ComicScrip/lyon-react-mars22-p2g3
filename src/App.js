0c20c73b1a6924e62ac8876aa9caa8cb4a250d30
dialloth <althierno@yahoo.fr>
2022-04-08 15:28:55 +0200

    css and html files modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './Css/App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import './Css/App.css';
 
 function App() {
   return (
