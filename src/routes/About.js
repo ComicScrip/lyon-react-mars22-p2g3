168f9771158160dfe3e967fa5c106c1339d0be00
Félix <fb.felixberger@gmail.com>
2022-04-11 17:07:46 +0200

    add presentation

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,7 +2,13 @@ import React from 'react';
 import '../Css/App.css';
 
 function AboutPage() {
-  return <h1 className="title"> Page about</h1>;
+  return (
+    <div>
+      <h1 className="title">About us</h1>
+      <p>Discover the Cinéchill team and meet cinema enthusiasts from multiple universes.</p>
+      <div>Carousel Image + Description</div>
+    </div>
+  );
 }
 
 export default AboutPage;
