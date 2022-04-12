4737744b96a682f5f15be58aac30aa0459dc9028
Anna Boval <anna.bo1106@gmail.com>
2022-04-12 16:09:37 +0200

    ok

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../routes/Home';
 import Contact from '../routes/Contact';
 import About from '../routes/About';
+import Details from '../routes/Details';
 import '../Css/App.css';
 
 export default function Main() {
@@ -11,6 +12,7 @@ export default function Main() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/details/Details" element={<Details />} />
       </Routes>
     </main>
   );
---
+++ src/routes/Details.jsx
@@ -0,0 +1,1 @@
+import '../Css/App.css';
