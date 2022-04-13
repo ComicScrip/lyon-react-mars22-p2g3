01df837123d35ce8da6dcf4d11224d7b8efd1145
Anna Boval <anna.bo1106@gmail.com>
2022-04-13 17:08:34 +0200

    added some more content on details.js and tried some css

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -1,25 +1,28 @@
 import '../Css/App.css';
+import '../Css/Details.css';
 
 export default function Details() {
   return (
     <div>
       <img
+        className="poster"
         src="https://imdb-api.com/images/original/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.6837_AL_.jpg"
         alt="movie"
       />
-      <h1>title</h1>
-      <p>releaseDate, runtimeStr, genres </p>
+      <h1> title</h1>
+      <p> releaseDate, runtimeStr, genres </p>
       <p> Director </p>
       <p> Rating </p>
       <h2>Synopsis</h2>
       <p>Plot</p>
+      <p>Cast</p>
+      <img className="cast" alt="cast" />
       <iframe
-        width="560"
-        height="315"
+        width="90%"
         src="https://www.youtube.com/embed/-FZ-pPFAjYY"
         title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        frameBorder="10"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; full-screen"
         allowFullScreen
       />{' '}
     </div>
