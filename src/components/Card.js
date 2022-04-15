6b94c1619f24c25ada4d2ad1fccc4e2088d61302
leolab69 <leolaborie123@gmail.com>
2022-04-15 14:12:46 +0200

    result page part 1

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,34 +5,49 @@ function Card() {
     <div>
       <h1 className="title-page">Result</h1>
       <div className="list-movie">
-        <div className="movie1">
+        <div className="movie">
           <img src="../img/poster.jpg" className="poster" alt="poster" />
           <div className="movie-details">
-            <div className="box">
-              <h3 className="title">Movie title</h3>
-              <p className="year">2016</p>
-              <h3 className="title">⋆ 9.4</h3>
-            </div>
+            <h3 className="title">Movie title</h3>
+            <p className="year">2016</p>
+            <h3 className="rating">
+              <img
+                src="../img/icons8-étoile-16.png"
+                className="icon"
+                alt="icon star"
+              />
+              9/10
+            </h3>
           </div>
         </div>
-        <div className="movie2">
+        <div className="movie">
           <img src="../img/poster.jpg" className="poster" alt="poster" />
           <div className="movie-details">
-            <div className="box">
-              <h3 className="title">Movie title</h3>
-              <p className="year">2016</p>
-              <h3 className="title">⋆ 9.4</h3>
-            </div>
+            <h3 className="title">Movie title</h3>
+            <p className="year">2016</p>
+            <h3 className="rating">
+              <img
+                src="../img/icons8-étoile-16.png"
+                className="icon"
+                alt="icon star"
+              />
+              9/10
+            </h3>
           </div>
         </div>
-        <div className="movie3">
+        <div className="movie">
           <img src="../img/poster.jpg" className="poster" alt="poster" />
           <div className="movie-details">
-            <div className="box">
-              <h3 className="title">Movie title</h3>
-              <p className="year">2016</p>
-              <h3 className="title">⋆ 9.4</h3>
-            </div>
+            <h3 className="title">Movie title</h3>
+            <p className="year">2016</p>
+            <h3 className="rating">
+              <img
+                src="../img/icons8-étoile-16.png"
+                className="icon"
+                alt="icon star"
+              />
+              9/10
+            </h3>
           </div>
         </div>
       </div>
