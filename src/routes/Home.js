2379a50c12732fb0c6ea97e09c8c9346d8562ca7
Félix <fb.felixberger@gmail.com>
2022-04-15 11:50:00 +0200

    last modification

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import '../Css/App.css';
 import '../Css/Home.css';
-import Caroussel from '../components/Caroussel';
 
 function HomePage() {
   return (
-    <>
-      <h1 className="title">
-        <span className="welcome">Welcome to</span> CINECHILL !
-      </h1>
-      <Caroussel />
-    </>
+    <h1 className="title">
+      <span className="welcome">Welcome to</span> CINECHILL !
+    </h1>
   );
 }
 
