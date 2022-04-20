05e80365e85a5a34892e4a7c0a77a2d8248958b8
leolab69 <leolaborie123@gmail.com>
2022-04-20 15:59:40 +0200

    wait pr

--- src/components/ResultAPI.js
+++ src/components/ResultAPI.js
@@ -5,9 +5,9 @@ function ResultAPI() {
   useEffect(() => {
     axios
       .get('https://imdb-api.com/en/API/Top250Movies/k_ns4o5sjc')
-      .then((res) => console.log.apply(res));
+      .then((res) => console.log(res.data.results));
   }, []);
-  return <div />;
+  return <div className="result" />;
 }
 
 export default ResultAPI;
