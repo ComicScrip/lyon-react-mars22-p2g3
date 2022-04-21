7fb065f44206eb8d0c11001118ddd1c3bec428ec
Félix <fb.felixberger@gmail.com>
2022-04-21 16:51:01 +0200

    create question component + style

---
+++ src/components/Question1.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import '../Css/App.css';
+import '../Css/Questionnaire.css';
+import '../Css/Navbar.css';
+import '../Css/index.css';
+
+function Question1() {
+  const [isActif, setIsActif] = useState(true);
+  const handleClick = () => {
+    setIsActif(isActif);
+  };
+
+  return (
+    <div>
+      <h2 className="question">What kind of movies do you like ?</h2>
+      <section className="section_card">
+        <div className="cardsx">
+          <button type="submit" className="cardy" onClick={handleClick}>
+            Comedy
+          </button>
+          <button type="submit" className="cardy">
+            Action
+          </button>
+          <button type="submit" className="cardy">
+            Sci-Fi
+          </button>
+          <button type="submit" className="cardy">
+            Drama
+          </button>
+          <button type="submit" className="cardy">
+            Thriller
+          </button>
+          <button type="submit" className="cardy">
+            Horror
+          </button>
+        </div>
+      </section>
+    </div>
+  );
+}
+export default Question1;
--- src/routes/Questionnaire.js
+++ src/routes/Questionnaire.js
@@ -1,39 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Css/App.css';
 import '../Css/Questionnaire.css';
 import '../Css/Navbar.css';
 import '../Css/index.css';
+// import Question1 from '../components/Question1';
+import Question2 from '../components/Question2';
 
 function Questionnaire() {
+  const [isActif, setIsActif] = useState(false);
+  const handleClick = () => {
+    setIsActif(!isActif);
+  };
   return (
     <div>
       <h1 className="question_title">QUESTIONS</h1>
+      <div className="cercle">
+        <span className="compteur"> 2/3</span>
+      </div>
       <h2 className="question">What kind of movies do you like ?</h2>
       <section className="section_card">
         <div className="cardsx">
-          <div className="cardy">
-            <h4 className="CategoryName">Comedy</h4>
-          </div>
-          <div className="cardy">
-            <h4 className="CategoryName">Action</h4>
-          </div>
-          <div className="cardy">
-            <h4 className="CategoryName">Sci-Fi</h4>
-          </div>
-          <div className="cardy">
-            <h4 className="CategoryName">Drama</h4>
-          </div>
-          <div className="cardy">
-            <h4 className="CategoryName">Thriller</h4>
-          </div>
-          <div className="cardy">
-            <h4 className="CategoryName">Horror</h4>
-          </div>
-        </div>
-        <div className="cercle">
-          <span className="compteur"> 1/2</span>
+          <button type="submit" className="cardy" onClick={handleClick}>
+            Comedy
+          </button>
+          <button type="submit" className="cardy" onClick={handleClick}>
+            Action
+          </button>
+          <button type="submit" className="cardy" onClick={handleClick}>
+            Sci-Fi
+          </button>
+          <button type="submit" className="cardy" onClick={handleClick}>
+            Drama
+          </button>
+          <button type="submit" className="cardy" onClick={handleClick}>
+            Thriller
+          </button>
+          <button type="submit" className="cardy" onClick={handleClick}>
+            Horror
+          </button>
         </div>
       </section>
+      <div>{isActif ? <Question2 /> : ''}</div>
     </div>
   );
 }
