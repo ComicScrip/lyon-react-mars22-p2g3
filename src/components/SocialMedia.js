0d4401517608f89af52350f16fcd75ca0757bea3
Anna Boval <anna.bo1106@gmail.com>
2022-04-24 17:06:51 +0200

    added react share with css effects

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,50 +1,68 @@
 import React from 'react';
 import '../Css/Details.css';
-import { FacebookShareButton, FacebookIcon } from 'react-share';
-import { Link } from 'react-router-dom';
+import {
+  FacebookShareButton,
+  FacebookMessengerShareButton,
+  WhatsappShareButton,
+  TwitterShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookMessengerIcon,
+  WhatsappIcon,
+  TwitterIcon,
+  EmailIcon,
+} from 'react-share';
 
 export default function SocialMedia() {
   return (
-    <div>
-      <div className="shareOn">
-        <div className="shareText">share on</div>
+    <div className="shareOn">
+      <div className="shareText" alt="share on social Media">
+        share on
+      </div>
+      <div className="shareLinks">
         <FacebookShareButton
-          url={'http://www.camperstribe.com'}
+          url={'http://localhost:3000/details'}
           quote={'CinéChill helps you to find the perfect movie !'}
           hashtag="#cinéchill"
-          className="share on Facebook"
+          className="shareOnFacebook"
         >
-          <FacebookIcon size={36} />
+          <FacebookIcon />
         </FacebookShareButton>
-        <Link to="//fr-fr.facebook.com/" target="_blank.">
-          <img
-            src="/pictures/icons8-facebook-messenger-30.png"
-            alt="share on Messenger"
-            className="shareOnMessenger"
-          />
-        </Link>
-        <Link to="//fr-fr.facebook.com/" target="_blank">
-          <img
-            src="/pictures/icons8-instagram-30.png"
-            alt="share on
-          Instagram"
-            className="shareOnInstagram"
-          />
-        </Link>
-        <Link to="//fr-fr.facebook.com/" target="_blank">
-          <img
-            src="/pictures/icons8-whatsapp-30.png"
-            alt="share on Messenger"
-            className="shareOnWhatsapp"
-          />
-        </Link>
-        <Link to="//fr-fr.facebook.com/" target="_blank">
-          <img
-            src="/pictures/icons8-twitter-30.png"
-            alt="share on Messenger"
-            className="shareOnTwitter"
-          />
-        </Link>
+
+        <FacebookMessengerShareButton
+          url={'http://localhost:3000/details'}
+          quote={'CinéChill helps you to find the perfect movie !'}
+          hashtag="#cinéchill"
+          className="shareOnMessenger"
+        >
+          <FacebookMessengerIcon />
+        </FacebookMessengerShareButton>
+
+        <WhatsappShareButton
+          url={'http://localhost:3000/details'}
+          quote={'CinéChill helps you to find the perfect movie !'}
+          hashtag="#cinéchill"
+          className="shareOnWhatsApp"
+        >
+          <WhatsappIcon />
+        </WhatsappShareButton>
+        <TwitterShareButton
+          url={'http://localhost:3000/details'}
+          quote={'CinéChill helps you to find the perfect movie !'}
+          hashtag="#cinéchill"
+          className="shareOnTwitter"
+        >
+          <TwitterIcon />
+        </TwitterShareButton>
+
+        <EmailShareButton
+          url={'http://localhost:3000/details'}
+          quote={'CinéChill helps you to find the perfect movie !'}
+          hashtag="#cinéchill"
+          className="shareByEmail"
+        >
+          <EmailIcon />
+        </EmailShareButton>
       </div>
     </div>
   );
