324feff4b1dcbc4d3f07991a89d384ddb7dd8f90
Werner Mona <wernermona96@gmail.com>
2022-04-28 15:25:34 +0200

    background video version descktop finit

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,15 +3,16 @@ import { Slide } from 'react-slideshow-image';
 import img1 from '../img/img1.jpg';
 import img2 from '../img/img2.jpg';
 import img3 from '../img/img3.jpg';
+import img4 from '../img/img4.jpg';
 import '../Css/Slider.css';
 import 'react-slideshow-image/dist/styles.css';
 
 const proprietes = {
-  duration: 5000,
+  duration: 3000,
   transitionDuration: 500,
   infinite: true,
-  indicators: true,
-  arrows: true,
+  indicators: false,
+  arrows: false,
 };
 
 function Slideshow() {
@@ -31,6 +32,11 @@ function Slideshow() {
         <div className="each-slide">
           <div>
             <img src={img3} alt="img3" />
+          </div>
+        </div>
+        <div className="each-slide">
+          <div>
+            <img src={img4} alt="img4" />
           </div>
         </div>
       </Slide>
