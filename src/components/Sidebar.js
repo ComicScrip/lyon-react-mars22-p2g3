74452058be1b2b2ca040ae62809494c8f91f5ffd
Werner Mona <wernermona96@gmail.com>
2022-04-29 09:36:23 +0200

    Spring finit, social links mise a jour

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../Css/Sidebar.css';
 
 function Sidebar() {
   return (
     <div className="social-container">
       <ul className="social-list">
-        <Link to="/about" className="icono">
-          <img src="../img/git.png" className="icons" alt="icons" />
-        </Link>
-        <Link to="/about">
-          <img src="../img/yt.png" className="icons" alt="icons" />
-        </Link>
-        <Link to="/about">
-          <img src="../img/fb.png" className="icons" alt="icons" />
-        </Link>
-        <Link to="/about">
-          <img src="../img/ig.png" className="icons" alt="icons" />
-        </Link>
+        <li>
+          <a
+            target="_blank"
+            href="https://github.com/ComicScrip/lyon-react-mars22-p2g3"
+            rel="noreferrer"
+          >
+            <img src="../img/git.png" className="social-icons" alt="git" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://www.instagram.com/wildcodeschoolofficial/?hl=fr"
+            rel="noreferrer"
+          >
+            <img src="../img/ig.png" className="social-icons" alt="instagram" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://fr-fr.facebook.com/WildCodeSchool/"
+            rel="noreferrer"
+          >
+            <img src="../img/fb.png" className="social-icons" alt="icons" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://www.youtube.com/watch?v=JfVOs4VSpmA"
+            rel="noreferrer"
+          >
+            <img src="../img/yt.png" className="social-icons" alt="youtube" />
+          </a>
+        </li>
       </ul>
     </div>
   );
